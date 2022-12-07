a26bfe355dd83ed454ca22ca63bf8d5aaac9b043
Atasay <atasayugras@gmail.com>
2022-12-07 16:36:11 +0300

    add mongoose.set('strictQuery', true);

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+mongoose.set('strictQuery', false);
 
 const app = express();
 
